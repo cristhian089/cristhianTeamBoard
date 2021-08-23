086e71576c20db400eb0f2868b178a953b0ef830
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-23 16:11:24 -0500

    adding functions list ,update ,register in role

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("./db/db");
 require("dotenv").config();
+const Role = require("./routes/role");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/role", Role);
+
 
 app.listen(process.env.PORT,()=>console.log("Backend server running on port: ", process.env.PORT));
 
---
+++ backend/routes/role.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const RoleController = require("../controllers/role");
+
+router.post(
+    "/registerRole",
+    RoleController.registerRole
+);
+
+router.get("/listRole",RoleController.listRole);
+router.put("/updateRole",RoleController.updateRole);
+
+module.exports = router;
+
