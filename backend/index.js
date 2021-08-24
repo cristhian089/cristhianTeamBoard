80ffa6fd5c31889dfaae94edc40c267e4e977909
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-24 17:21:27 -0500

    adding middleware

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ const cors = require("cors");
 const { dbConnection } = require("./db/db");
 require("dotenv").config();
 const Role = require("./routes/role");
+const User = require("./routes/user");
+const Board = require("./routes/board");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/role", Role);
+app.use("/api/user", User);
+app.use("/api/board", Board);
+app.use("/uploads",express.static("uploads"));
 
 
 app.listen(process.env.PORT,()=>console.log("Backend server running on port: ", process.env.PORT));
