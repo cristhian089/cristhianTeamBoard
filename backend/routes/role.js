0bc53aa58fbc5a3d825fd4645b1b3a50d2a15802
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-24 17:30:32 -0500

    modified route role.js

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const RoleController = require("../controllers/role");
+const Auth = require("../middleware/auth");
+const ValidateUser = require("../middleware/validateUser");
+const Admin = require("../middleware/admin");
 
 router.post(
     "/registerRole",
+    Auth,
+    ValidateUser,
+    Admin,
     RoleController.registerRole
 );
 
-router.get("/listRole",RoleController.listRole);
-router.put("/updateRole",RoleController.updateRole);
+router.get("/listRole",Auth, ValidateUser, Admin,RoleController.listRole);
+router.put("/updateRole",Auth, ValidateUser, Admin,RoleController.updateRole);
 
 module.exports = router;
 
