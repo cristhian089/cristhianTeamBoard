22de5ef617960d8420480f20ddac8b66b8f7dbac
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-25 21:16:30 -0500

    adding components footer, header, register

--- frontend/src/app/home/register/register.component.ts
+++ frontend/src/app/home/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../services/user.service";
+import { Router } from "@angular/router";
+import { MatSnackBar, MatSnackBarHorizontalPosition,MatSnackBarVerticalPosition, } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerData:any;
+  message: string;
+  horizontalPosition:MatSnackBarHorizontalPosition ='end';
+  verticalPosition:MatSnackBarVerticalPosition = 'top';
 
-  constructor() { }
+  constructor(private _userService: UserService, private _router: Router, private _snackBar:MatSnackBar) { 
+    this.message='';
+    this.registerData={};
+  }
 
   ngOnInit(): void {
   }
+
+  registerUser(){}
+
+  openSnackBarSuccesfull(){}
+
+  openSnackBarError(){}
 
 }
