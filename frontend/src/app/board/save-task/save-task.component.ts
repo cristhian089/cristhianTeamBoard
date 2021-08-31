f3fbe337874648d8976c8f9e4d3a700a77feef79
Cristhian Zambrano <cz4785464@gmail.com>
2021-08-31 16:22:28 -0500

    adding saveTaskImg

--- frontend/src/app/board/save-task/save-task.component.ts
+++ frontend/src/app/board/save-task/save-task.component.ts
@@ -19,12 +19,14 @@ export class SaveTaskComponent implements OnInit {
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
   durationInSeconds: number = 2;
+  selectedFile:any;
 
   constructor(private _boardService: BoardService,
     private _router: Router,
     private _snackBar: MatSnackBar) {
       this.message = '';
       this.registerData = {};
+      this.selectedFile = null
      }
 
   ngOnInit(): void {
@@ -41,6 +43,41 @@ export class SaveTaskComponent implements OnInit {
       this.registerData = {};
     } else {
       this._boardService.saveTask(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          this._router.navigate(['/listTask']);
+          this.message = 'Task create';
+          this.openSnackBarSuccesfull();
+          this.registerData = {};
+        },
+        (err) => {
+          console.log(err);
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      );
+    }
+  }
+  uploadImg(event: any){
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  saveTaskImg(){
+    if (
+      !this.registerData.name ||
+      !this.registerData.description
+    ) {
+      console.log('Failed process: Incomplete data');
+      this.message = 'Failed process: Incomplete data';
+      this.openSnackBarError();
+      this.registerData = {};
+    } else {
+      const data = new FormData();
+      data.append('image',this.selectedFile, this.selectedFile.name)
+      data.append('name',this.registerData.name)
+      data.append('description',this.registerData.description)
+
+      this._boardService.saveTaskImg(data).subscribe(
         (res) => {
           console.log(res);
           this._router.navigate(['/listTask']);
